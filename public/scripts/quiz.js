759cda15220fa350ea604eb48378bf813236a1b4
KarolJaskolka <jaskolka.karol@gmail.com>
2019-08-16 16:39:50 +0200

    Add data/quiz.json

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,59 +1,44 @@
-const test = [
-    {
-        question:"How old is Crisitano Ronaldo?",
-        option1:"32",
-        option2:"33",
-        option3:"34",
-        option4:"35",
-        answer:"3"
-    },
-    {
-        question:"Which club won the Uefa Champions League most times?",
-        option1:"FC Barcelona",
-        option2:"Real Madryt",
-        option3:"Liverpool FC",
-        option4:"AC Milan",
-        answer:"2"
-    },
-    {
-        question:"Who won Ballon'Dor in 2018?",
-        option1:"Cristiano Ronaldo",
-        option2:"Leo Messi",
-        option3:"Kylian Mbappe",
-        option4:"Luka Modric",
-        answer:"4"
-    }
-];
+let elQuestion = document.getElementById('question');
+let elOptions = document.getElementsByTagName('li');
+let elScore = document.getElementById('score');
+let elButton = document.getElementById('button');
     
-var elQuestion = document.getElementById('question');
-var elOptions = document.getElementsByTagName('li');
-var elScore = document.getElementById('score');
-var elButton = document.getElementById('button');
-    
-var current = 0;
-var score = 0;
-var selectedAnswer = 0;
-var checked = false;
+let current = 0;
+let score = 0;
+let selectedAnswer = 0;
+let checked = false;
+
+let test;
+
+const request = new XMLHttpRequest();
+
+request.onload = () => { 
+    test = JSON.parse(request.responseText); 
+    loadQuestion();
+}
+
+request.open('GET','data/quiz.json',true);
+request.send(null);
     
 function loadQuestion(){
-    var items = test[current];
+    
+    const items = test.test[current];
     elScore.textContent = 'Score : ' + score;
     elQuestion.textContent = items.question;
-    
-    elOptions[0].textContent = items.option1; 
-    elOptions[1].textContent = items.option2;
-    elOptions[2].textContent = items.option3;
-    elOptions[3].textContent = items.option4;
 
-    for(var i=0; i<4; i++){
-        elOptions[i].className = 'basic';
+    for(let i=0; i<4; i++){
+        elOptions[i].textContent = items.options[i];
     }
 
+    for(let i=0; i<4; i++){
+        elOptions[i].className = 'basic';
+    }
+    
 }   
     
 function select(e, number){
-    var target = e.target;
-    for(var i=0; i<4; i++){
+    const target = e.target;
+    for(let i=0; i<4; i++){
         elOptions[i].className = 'basic';
     }
     target.className = 'checked';
@@ -61,17 +46,17 @@ function select(e, number){
 }
     
 function check(){
-    var items = test[current];
+    const items = test.test[current];
     if(selectedAnswer == items.answer){
         score++;
         elScore.textContent = 'Score : ' + score;
     }
     else{
         if(selectedAnswer){
-            elOptions[selectedAnswer-1].className = 'false';
+            elOptions[selectedAnswer].className = 'false';
         }
     }
-    elOptions[items.answer-1].className = 'true';
+    elOptions[items.answer].className = 'true';
     checked = true;
     elButton.value = 'Next';
     
@@ -99,20 +84,12 @@ function action(){
         check();
     }
 }
-    
-elOptions[0].addEventListener('click',function(e){
-    select(e,1);
-},false)
-elOptions[1].addEventListener('click',function(e){
-    select(e,2);
-},false)
-elOptions[2].addEventListener('click',function(e){
-    select(e,3);
-},false)
-elOptions[3].addEventListener('click',function(e){
-    select(e,4);
-},false)
+
+for(let i=0; i<4; i++){
+    elOptions[i].addEventListener('click',function(e){
+        select(e,i);
+    },false)
+}
 
 elButton.addEventListener('click',action,false);
 
-loadQuestion();
